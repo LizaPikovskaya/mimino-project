7473f820c142509160341bdeb9815855251b80e8
olha-smieshko <smeshkoom@gmail.com>
2023-02-03 00:06:58 +0200

    календарики

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,8 @@
 !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
-
+document.getElementById('checkin_data').valueAsDate = new Date();
+document.getElementById('checkout_data').valueAsDate = new Date();
+document.getElementById('table_data').valueAsDate = new Date();
 document.addEventListener('DOMContentLoaded', function() {
 
    /* Записываем в переменные массив элементов-кнопок и подложку.
@@ -94,3 +96,4 @@ document.addEventListener('DOMContentLoaded', function() {
     
 
 }); // end ready
+
