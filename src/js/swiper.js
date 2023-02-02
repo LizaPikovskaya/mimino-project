ed009ddbc4eb3c65412068a5d344be287419aa80
YazikovXIII <vladzaraza@gmail.com>
2023-02-02 22:52:22 +0200

    swipers-main

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,30 +1,86 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+//hotel-room
+var swiper = new Swiper('.js-swiper-hotel-room', {
   direction: 'horizontal',
   loop: true,
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-  
-  // Navigation arrows
+  autoplay: {
+  delay: 2000,
+  disableOnInteraction: true,
+  },
   navigation: {
     nextEl: '.s-button-next',
     prevEl: '.s-button-prev',
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
 
-  var swiper1 = new Swiper(".swiper__main", {
-      loop: true,
-      slidesPerView: 2,
-      spaceBetween: 5,
-      navigation: {
-          nextEl: '.s-button-next',
-          prevEl: '.s-button-prev',
-      },
-  });
+var swiper1 = new Swiper(".swiper__main", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 5,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+
+  navigation: {
+    nextEl: '.s-button-next-1',
+    prevEl: '.s-button-prev-1',
+  },
+});
+
+var swiper1 = new Swiper(".swiper__main_1", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 5,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+
+  navigation: {
+    nextEl: '.s-button-next-about-rest',
+    prevEl: '.s-button-prev-about-rest',
+  },
+});
+
+var swiper2 = new Swiper(".swiper__main_2", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 5,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+
+  navigation: {
+    nextEl: '.s-button-next-det-rest',
+    prevEl: '.s-button-prev-det-rest',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+});
+
+var swiper3 = new Swiper(".swiper__main_3", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 5,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+
+  navigation: {
+    nextEl: '.s-button-next-about-hotel',
+    prevEl: '.s-button-prev-about-hotel',
+  },
+});
